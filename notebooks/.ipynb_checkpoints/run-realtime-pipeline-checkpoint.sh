6ffb38942158962dbab0a1724a62e53fe08d1604
Atef-Mousa <atefatef.mousa37@gmail.com>
2025-08-10 18:28:49 +0300

    removing unused data

---
+++ notebooks/.ipynb_checkpoints/run-realtime-pipeline-checkpoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+pip install papermill
+
+
+pip install ipykernel
+
+PID_FILE="pipeline.pids"
+> "$PID_FILE"  # Clear previous PIDs
+
+papermill /home/jovyan/work/kafka-connections/Topic.ipynb /home/jovyan/work/kafka-connections/Topic.ipynb -k python3 --log-output
+
+
+# Run Kafka producer
+
+papermill /home/jovyan/work/kafka-connections/producer.ipynb /home/jovyan/work/kafka-connections/producer.ipynb -k python3 --log-output &
+echo $! > "$PID_FILE"     # Save PID of producer
+
+# Run Spark consumer
+papermill /home/jovyan/work/Spark-Streaming/Cleaning_Treansformations.ipynb /home/jovyan/work/Spark-Streaming/Cleaning_Treansformations.ipynb -k python3 --log-output &
+echo $! >> "$PID_FILE"    # Save PID of consumer
+
+echo "Producer and Consumer started. PIDs saved in $PID_FILE"
